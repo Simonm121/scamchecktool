556aa4b4ae119e17d4277f41fbf0ac94657a6f93
Simon Miller <simonmillerhome@yahoo.com>
2026-05-06 18:39:34 +0100

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import "./globals.css";
 
 import { PageShell } from "@/components/site/page-shell";
 import { siteConfig } from "@/lib/site";
+
+const googleAnalyticsId = "G-5X7SSJD8Q6";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
@@ -23,6 +26,20 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <PageShell>{children}</PageShell>
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${googleAnalyticsId}');
+          `}
+        </Script>
       </body>
     </html>
   );
